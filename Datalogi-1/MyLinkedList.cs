ad7b51cd0ea20c990c37f8fe7960ad9ae634246d
David Andersson <lejonmanen@gmail.com>
2022-06-07 14:21:06 +0200

    Länkad lista

---
+++ Datalogi-1/MyLinkedList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datalogi_1
+{
+    public class MyLinkedList<T>
+    {
+        private MyNode<T>? Head = null;
+        // lägg till Current
+        public void Push(T value) {
+            MyNode<T> oldHead = Head;
+            this.Head = new MyNode<T>();
+            this.Head.Data = value;
+            this.Head.Next = oldHead;
+        }
+
+        public T Get(int index) {
+            var nodeReference = this.Head;
+            for(int i = 0; i < index; i++)
+            {
+                nodeReference = nodeReference.Next;
+            }
+            return nodeReference.Data;
+        }
+    }
+    public class MyNode<T>
+    {
+        public T Data { get; set; }
+        public MyNode<T> Next { get; set; }
+    }
+}
--- Datalogi-1/Program.cs
+++ Datalogi-1/Program.cs
@@ -1,15 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Datalogi_1;
 using System.Diagnostics;
 
 Console.WriteLine("Hello, World!");
 
-for (int i = 0; i < 5; i++)
+/*for (int i = 0; i < 5; i++)
 {
     Övning1a();
 }
 for (int i = 0; i < 5; i++)
 {
     Övning1b();
+}*/
+for(int i=0; i < 5; i++)
+{
+    Övning3();
 }
 
 static void Övning1a()
@@ -49,4 +54,48 @@ static void Övning1b()
     // stoppa tidtagning, skriv ut resultatet
     timer.Stop();
     Console.WriteLine("1b Elapsed time: " + timer.ElapsedMilliseconds);
+}
+
+static void Övning2() {
+    const int count = 180000000;
+    Stopwatch timer = Stopwatch.StartNew();
+    List<double> list = new();
+    Random random = new Random();
+    for (int i = 0; i < count; i++)
+    {
+        list.Add(random.NextDouble() * 100 + 100);
+    }
+    timer.Stop();
+    Console.WriteLine("2a Elapsed time: " + timer.ElapsedMilliseconds);
+
+    timer = Stopwatch.StartNew();
+    double sum = 0;
+    for (int i = 0; i < count; i++)
+    {
+        sum += list[i];
+    }
+    timer.Stop();
+    Console.WriteLine("2b Elapsed time: " + timer.ElapsedMilliseconds);
+}
+static void Övning3()
+{
+    const int count = 72000;
+    Stopwatch timer = Stopwatch.StartNew();
+    MyLinkedList<double> list = new();
+    Random random = new Random();
+    for (int i = 0; i < count; i++)
+    {
+        list.Push(random.NextDouble() * 100 + 100);
+    }
+    timer.Stop();
+    Console.WriteLine("3a Elapsed time: " + timer.ElapsedMilliseconds);
+
+    timer = Stopwatch.StartNew();
+    double sum = 0;
+    for (int i = 0; i < count; i++)
+    {
+        sum += list.Get(i);
+    }
+    timer.Stop();
+    Console.WriteLine("3b Elapsed time: " + timer.ElapsedMilliseconds);
 }
