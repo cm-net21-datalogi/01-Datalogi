713b9ca1a9aa121639d98aa9720bbaca1779387d
David Andersson <david.andersson@zocom.se>
2022-06-14 10:54:07 +0200

    Lösningsförslag removeAt

--- Datalogi-1/MyStack.cs
+++ Datalogi-1/MyStack.cs
@@ -82,6 +82,66 @@ namespace Datalogi_1
 		{
 			Pop();
 		}
+
+		public void RemoveAt(int index)
+		{
+			// Vi behöver göra olika saker beroende på hur många noder det finns i listan
+			// Kontrollera index - får inte vara negativt
+			// Kan bara fortsätta om listan inte är tom
+
+			// Listan har 1 nod
+			// Listan har 2 noder
+			// osv.
+
+			if (index < 0)
+			{
+				throw new Exception("Index måste vara minst noll.");
+			}
+			else if (Top == null)
+			{
+				throw new Exception("Stacken är tom, det går inte att ta bort några noder.");
+			}
+			else if (Top.Next == null && index == 0)
+			{
+				Top = null;
+				Last = null;
+			}
+			else if (Top.Next == null && index > 0)
+			{
+				throw new Exception("Kan inte ta bort, stacken har inte så många element.");
+			}
+			else if( index== 0)
+			{
+				Top = Top.Next;
+				if (Top == null) Last = null;
+			} else
+			{
+				MyNode<T> node = Top;
+				while (index > 1)
+				{
+					node = node.Next;
+					index--;
+					if (node == null)
+					{
+						throw new Exception("Kan inte ta bort, stacken har inte så många element.");
+					}
+				}
+				// Nu ska vi ta bort noden som "node.Next" refererar till
+				if (node.Next == null)
+				{
+					throw new Exception("Kan inte ta bort, stacken har inte så många element.");
+				}
+				node.Next = node.Next.Next;
+			}
+			// index 0 är första noden
+			// index 1 är andra noden
+			// osv...
+			// index 500000 är nod nr 500001
+			/*RemoveAt(500000);  // inte säkert att det finns 500000 noder
+			RemoveAt(1);  // kanske inte finns 2
+			RemoveAt(0);  // kanske inte ens finns 1
+			RemoveAt(-1); // aldrig okej */
+		}
 	}
 	// Vi behöver spara både datan och en referens till nästa nod, vi använder MyNode för det
 	/*public class MyNode<T>
