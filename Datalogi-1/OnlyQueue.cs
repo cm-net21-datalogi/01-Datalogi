b8883563dc6799368163a1fafd2a9b4ba4bfdede
David Andersson <david.andersson@zocom.se>
2022-06-14 13:59:54 +0200

    Lösningsförslag separat Stack och Queue

---
+++ Datalogi-1/OnlyQueue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datalogi_1
+{
+	public class OnlyQueue<T>:ISimpleQueue<T>
+	{
+		private MyNode<T>? First = null;
+		private MyNode<T>? Last = null;
+
+		public void AddLast(T value)
+		{
+			var oldLast = Last;
+			Last = new MyNode<T>();
+			Last.Data = value;
+			if (First == null)
+				First = Last;
+			if(oldLast != null)
+				Last.Next = oldLast;
+		}
+
+		public T GetFirst()
+		{
+			if (First == null)
+				throw new Exception("Kön är tom");
+			return First.Data;
+		}
+
+		public void RemoveFirst()
+		{
+			var node = Last;
+			if (node == null)
+				throw new Exception("Kön är tom");
+
+			// hitta näst första noden
+			while(node.Next != First)
+			{
+				node = node.Next;
+			}
+			node.Next = null;
+			First=node;
+
+		}
+	}
+}
---
+++ Datalogi-1/OnlyStack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datalogi_1
+{
+	public class OnlyStack<T> : ISimpleStack<T>
+	{
+		private MyNode<T>? Head = null;
+		public T Peek()
+		{
+			if (Head == null)
+				throw new Exception("Stacken är tom");
+			else
+				return Head.Data;
+		}
+
+		public T Pop()
+		{
+			if (Head == null)
+				throw new Exception("Stacken är tom");
+			var data = Head.Data;
+			Head = Head.Next;
+			return data;
+		}
+
+		public void Push(T value)
+		{
+			var oldHead = Head;
+			Head = new MyNode<T>();
+			Head.Data = value;
+			Head.Next = oldHead;
+		}
+	}
+}
